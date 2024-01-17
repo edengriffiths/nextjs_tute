e5c54c460f123086de19beefaba122f5dfcba2fb
Eden Griffiths <edengriffy@gmail.com>
2024-01-17 14:37:15 +1100

    tute chap 1-5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
